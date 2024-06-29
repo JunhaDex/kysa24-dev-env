ee6c4d2ce3074ab66ada2a2fc1027e08b85e3146
JunhaDex <dk.junha.dev@gmail.com>
2024-06-30 01:02:46 +0900

    update test seeds

--- storage/mysql/seed/chat-seed.sql
+++ storage/mysql/seed/chat-seed.sql
@@ -1,15 +1,15 @@
-INSERT INTO service.chat_room (id, ref, members, created_at, updated_at)
-VALUES (1, '16a9f327-38d1-4832-b109-dd32f85fd0ee', '[
-  1,
-  2
-]', '2024-06-27 22:40:19', '2024-06-27 22:40:19');
+INSERT INTO service.chat_room (ref, members)
+VALUES ('16a9f327-38d1-4832-b109-dd32f85fd0ee', '[
+  10001,
+  10002
+]');
 
-INSERT INTO service.chat_room_view (id, user_id, room_id, title, is_block, last_read, created_at, updated_at)
-VALUES (2, 10001, 1, '준수닉', 0, 0, '2024-06-27 22:41:10', '2024-06-27 22:41:10');
-INSERT INTO service.chat_room_view (id, user_id, room_id, title, is_block, last_read, created_at, updated_at)
-VALUES (3, 10002, 1, '뉴진스민지', 0, 0, '2024-06-27 22:41:10', '2024-06-27 22:41:10');
+INSERT INTO service.chat_room_view (user_id, room_id, title, is_block, last_read, created_at, updated_at)
+VALUES (10001, 1, '준수닉', 0, 0, '2024-06-27 22:41:10', '2024-06-27 22:41:10');
+INSERT INTO service.chat_room_view (user_id, room_id, title, is_block, last_read, created_at, updated_at)
+VALUES (10002, 1, '뉴진스민지', 0, 0, '2024-06-27 22:41:10', '2024-06-27 22:41:10');
 
-INSERT INTO service.chat (id, room_id, sender, message, encoded, created_at, updated_at)
-VALUES (2, 1, 10001, '안녕하세요 반가워요!', 0, '2024-06-27 22:43:53', '2024-06-27 22:43:53');
-INSERT INTO service.chat (id, room_id, sender, message, encoded, created_at, updated_at)
-VALUES (3, 1, 10002, '네 반가워요! 친하게 지내요!', 0, '2024-06-27 22:43:53', '2024-06-27 22:43:53');
+INSERT INTO service.chat (room_id, sender, message, encoded, created_at, updated_at)
+VALUES (1, 10001, '안녕하세요 반가워요!', 0, '2024-06-27 22:43:53', '2024-06-27 22:43:53');
+INSERT INTO service.chat (room_id, sender, message, encoded, created_at, updated_at)
+VALUES (1, 10002, '네 반가워요! 친하게 지내요!', 0, '2024-06-27 22:45:53', '2024-06-27 22:45:53');
--- storage/mysql/seed/user-seed.sql
+++ storage/mysql/seed/user-seed.sql
@@ -1,2 +1,19 @@
-INSERT INTO service.user (id, ref, name, sex, age, dob, nickname, auth_id, pwd, team_id, profile_img, cover_img, introduce, geo, act_status, created_at, updated_at) VALUES (10001, '7d79e3ee-c81f-4743-861c-c336bbc780ad', '김민지', 2, 20, '2004-05-07 00:00:00', '뉴진스민지', 'new.minji', '$2b$10$6ra8OeiwWPaxTeFs2.QNl.czN3KLMOwo3.taLT3IBpA7Ksu6ffndS', 13, '', '', '', '강원 춘천', 1, '2024-06-27 22:39:32', '2024-06-27 22:39:32');
-INSERT INTO service.user (id, ref, name, sex, age, dob, nickname, auth_id, pwd, team_id, profile_img, cover_img, introduce, geo, act_status, created_at, updated_at) VALUES (10002, '7549143a-82bb-4dea-804a-096337cacf6f', '박준수', 1, 29, '1995-04-17 00:00:00', '준수닉', 'park.jun', '$2b$10$ZovJwBkMJHtGBbE1LJuThuArn8iA.cYjc307Ufwfsv2MnX2LgeiKe', 3, '', '', '', '부산 해운대', 1, '2024-06-27 22:39:45', '2024-06-27 22:39:45');
+INSERT INTO service.user (id, ref, name, sex, age, dob, nickname, auth_id, pwd, team_id, profile_img, cover_img,
+                          introduce, geo, act_status)
+VALUES (10001, '7d79e3ee-c81f-4743-861c-c336bbc780ad', '김민지', 2, 20, '2004-05-07 00:00:00', '뉴진스민지', 'new.minji',
+        '$2b$10$6ra8OeiwWPaxTeFs2.QNl.czN3KLMOwo3.taLT3IBpA7Ksu6ffndS', 13, '', '', '', '강원 춘천', 1),
+       (10002, '7549143a-82bb-4dea-804a-096337cacf6f', '박준수', 1, 29, '1995-04-17 00:00:00', '준수닉', 'park.jun',
+        '$2b$10$ZovJwBkMJHtGBbE1LJuThuArn8iA.cYjc307Ufwfsv2MnX2LgeiKe', 3, '', '', '', '부산 해운대', 1),
+       (10003, 'ec78a521-74f2-4b63-9160-33eda5c03fe1', '김철수', 1, 25, '1999-01-01 00:00:00', '철수닉', 'kim.chul',
+        '$2b$10$6ra8OeiwWPaxTeFs2.QNl.czN3KLMOwo3.taLT3IBpA7Ksu6ffndS', 11, '', '', '', '서울 강남', 1),
+       (10004, '29786573-95b7-4edc-a9bb-f96036303176', '이영희', 2, 30, '1994-01-01 00:00:00', '영희닉', 'lee.young',
+        '$2b$10$6ra8OeiwWPaxTeFs2.QNl.czN3KLMOwo3.taLT3IBpA7Ksu6ffndS', 22, '', '', '', '서울 서초', 1),
+       (10005, '6ced39ba-ea6e-4436-9af8-15bf68ce3517', '박준영', 1, 21, '2003-01-01 00:00:00', '준영닉', 'park.juny',
+        '$2b$10$6ra8OeiwWPaxTeFs2.QNl.czN3KLMOwo3.taLT3IBpA7Ksu6ffndS', 3, '', '', '', '부산 해운대', 1),
+       (10006, '889eb737-68f3-42f9-a351-81d408d200e7', '최은지', 2, 22, '2002-01-01 00:00:00', '은지닉', 'choi.eunji',
+        '$2b$10$6ra8OeiwWPaxTeFs2.QNl.czN3KLMOwo3.taLT3IBpA7Ksu6ffndS', 24, '', '', '', '대구 수성', 1),
+       (10007, '20b2d7eb-3a82-43d8-814e-f4c302c5892a', '김태현', 1, 24, '2000-01-01 00:00:00', '태현닉', 'kim.tae',
+        '$2b$10$6ra8OeiwWPaxTeFs2.QNl.czN3KLMOwo3.taLT3IBpA7Ksu6ffndS', 5, '', '', '', '인천 연수', 1),
+       (10008, 'b31c6185-9d35-451e-b5fd-e740d60c2765', '이소연', 2, 25, '1999-01-01 00:00:00', '소연닉', 'lee.soyeon',
+        '$2b$10$6ra8OeiwWPaxTeFs2.QNl.czN3KLMOwo3.taLT3IBpA7Ksu6ffndS', 16, '', '', '', '광주 서구', 1);
+
